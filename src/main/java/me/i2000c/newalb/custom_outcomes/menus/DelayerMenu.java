e84ea26be73e3e83f96a9cab65b6c6769b74e534
I2000C <prueb123i@outlook.es>
2022-08-07 19:12:17 +0200

    Updated DelayerMenu.java

--- src/main/java/me/i2000c/newalb/custom_outcomes/menus/DelayerMenu.java
+++ src/main/java/me/i2000c/newalb/custom_outcomes/menus/DelayerMenu.java
@@ -1,19 +1,17 @@
 package me.i2000c.newalb.custom_outcomes.menus;
 
 import com.cryptomorin.xseries.XMaterial;
-import me.i2000c.newalb.NewAmazingLuckyBlocks;
 import me.i2000c.newalb.custom_outcomes.utils.rewards.Reward;
-import me.i2000c.newalb.utils.logger.Logger;
-import java.util.Arrays;
 import me.i2000c.newalb.listeners.inventories.CustomInventoryType;
 import me.i2000c.newalb.listeners.inventories.GUIFactory;
+import me.i2000c.newalb.listeners.inventories.GUIItem;
+import me.i2000c.newalb.listeners.inventories.GlassColor;
 import me.i2000c.newalb.listeners.inventories.InventoryFunction;
 import me.i2000c.newalb.listeners.inventories.InventoryListener;
-import org.bukkit.Material;
+import me.i2000c.newalb.utils2.ItemBuilder;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class DelayerMenu{
     public static Reward reward;
@@ -38,55 +36,16 @@ public class DelayerMenu{
         Inventory inv = GUIFactory.createInventory(CustomInventoryType.DELAYER_MENU, 27, "&5Configure Delay");
         GUIManager.setCurrentInventory(inv);
         
-        ItemStack glass = XMaterial.BLUE_STAINED_GLASS_PANE.parseItem();
-        ItemMeta meta = glass.getItemMeta();
-        meta.setDisplayName(" ");
-        glass.setItemMeta(meta);
+        ItemStack glass = GUIItem.getGlassItem(GlassColor.BLUE);
         
         if(delayValue < 0){
             delayValue = reward.getDelay();
         }
-        ItemStack delay = XMaterial.CLOCK.parseItem();
-        meta = delay.getItemMeta();
-        meta.setDisplayName("&6Delay: &b" + delayValue + " &dtick(s)");
-        meta.setLore(Arrays.asList("        &a" + delayValue/20.0 + " &dsecond(s)", "&3Click to reset"));
-        delay.setItemMeta(meta);
-        
-        ItemStack plus = XMaterial.LIME_STAINED_GLASS_PANE.parseItem();
-        meta = plus.getItemMeta();
-        meta.setDisplayName("&a&l+1");
-        plus.setItemMeta(meta);
-        ItemStack plus10 = plus.clone();
-        meta = plus10.getItemMeta();
-        meta.setDisplayName("&a&l+10");
-        plus10.setItemMeta(meta);
-        ItemStack plus100 = plus.clone();
-        meta = plus100.getItemMeta();
-        meta.setDisplayName("&a&l+100");
-        plus100.setItemMeta(meta);
-        
-        ItemStack less = XMaterial.RED_STAINED_GLASS_PANE.parseItem();
-        meta = less.getItemMeta();
-        meta.setDisplayName("&c&l-1");
-        less.setItemMeta(meta);
-        ItemStack less10 = less.clone();
-        meta = less10.getItemMeta();
-        meta.setDisplayName("&c&l-10");
-        less10.setItemMeta(meta);
-        ItemStack less100 = less.clone();
-        meta = less100.getItemMeta();
-        meta.setDisplayName("&c&l-100");
-        less100.setItemMeta(meta);
-        
-        ItemStack back = new ItemStack(Material.ENDER_PEARL);
-        meta = back.getItemMeta();
-        meta.setDisplayName("&7Back");
-        back.setItemMeta(meta);
-        
-        ItemStack next = new ItemStack(Material.ANVIL);
-        meta = next.getItemMeta();
-        meta.setDisplayName("&bNext");
-        next.setItemMeta(meta);
+        ItemStack delay = ItemBuilder.newItem(XMaterial.CLOCK)
+                .withDisplayName("&6Delay: &b" + delayValue + " &dtick(s)")
+                .addLoreLine("        &a" + delayValue/20.0 + " &dsecond(s)")
+                .addLoreLine("&3Click to reset")
+                .build();
         
         for(int i=0;i<=9;i++){
             inv.setItem(i, glass);
@@ -95,18 +54,18 @@ public class DelayerMenu{
             inv.setItem(i, glass);
         }
         
-        inv.setItem(10, back);
-        inv.setItem(16, next);
+        inv.setItem(10, GUIItem.getBackItem());
+        inv.setItem(16, GUIItem.getNextItem());
         
         inv.setItem(13, delay);
         
-        inv.setItem(3, plus);
-        inv.setItem(4, plus10);
-        inv.setItem(5, plus100);
+        inv.setItem(3, GUIItem.getPlusLessItem(+1));
+        inv.setItem(4, GUIItem.getPlusLessItem(+10));
+        inv.setItem(5, GUIItem.getPlusLessItem(+100));
         
-        inv.setItem(21, less);
-        inv.setItem(22, less10);
-        inv.setItem(23, less100);
+        inv.setItem(21, GUIItem.getPlusLessItem(-1));
+        inv.setItem(22, GUIItem.getPlusLessItem(-10));
+        inv.setItem(23, GUIItem.getPlusLessItem(-100));
         
         p.openInventory(inv);
 //</editor-fold>
