2300cf7f108a661971d403a7df5a73ad58633675
I2000C <prueb123i@outlook.es>
2022-08-09 17:29:12 +0200

    Updated SoundMenu.java

--- src/main/java/me/i2000c/newalb/custom_outcomes/menus/SoundMenu.java
+++ src/main/java/me/i2000c/newalb/custom_outcomes/menus/SoundMenu.java
@@ -1,24 +1,25 @@
 package me.i2000c.newalb.custom_outcomes.menus;
 
 import com.cryptomorin.xseries.XMaterial;
-import me.i2000c.newalb.NewAmazingLuckyBlocks;
-import me.i2000c.newalb.custom_outcomes.utils.rewards.SoundReward;
-import me.i2000c.newalb.utils.logger.Logger;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import me.i2000c.newalb.MinecraftVersion;
+import me.i2000c.newalb.NewAmazingLuckyBlocks;
+import me.i2000c.newalb.custom_outcomes.utils.rewards.SoundReward;
 import me.i2000c.newalb.listeners.inventories.CustomInventoryType;
 import me.i2000c.newalb.listeners.inventories.GUIFactory;
+import me.i2000c.newalb.listeners.inventories.GUIItem;
+import me.i2000c.newalb.listeners.inventories.GlassColor;
 import me.i2000c.newalb.listeners.inventories.InventoryFunction;
 import me.i2000c.newalb.listeners.inventories.InventoryListener;
-import me.i2000c.newalb.MinecraftVersion;
-import org.bukkit.Material;
+import me.i2000c.newalb.utils.logger.Logger;
+import me.i2000c.newalb.utils2.ItemBuilder;
 import org.bukkit.Sound;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class SoundMenu{
     public static SoundReward reward;
@@ -47,10 +48,7 @@ public class SoundMenu{
         Inventory inv = GUIFactory.createInventory(CustomInventoryType.SOUND_MENU, 27, "&d&lSound Reward");
         GUIManager.setCurrentInventory(inv);
         
-        ItemStack glass = XMaterial.MAGENTA_STAINED_GLASS_PANE.parseItem();
-        ItemMeta meta = glass.getItemMeta();
-        meta.setDisplayName(" ");
-        glass.setItemMeta(meta);
+        ItemStack glass = GUIItem.getGlassItem(GlassColor.MAGENTA);
         
         for(int i=0;i<9;i++){
             inv.setItem(i, glass);
@@ -61,59 +59,39 @@ public class SoundMenu{
         inv.setItem(9, glass);
         inv.setItem(17, glass);
         
-        ItemStack sound = new ItemStack(Material.NOTE_BLOCK);
-        meta = sound.getItemMeta();
+        ItemBuilder builder = ItemBuilder.newItem(XMaterial.NOTE_BLOCK);
         if(reward.getType() == null){
-            meta.setDisplayName("&6Select sound type");
+            builder.withDisplayName("&6Select sound type");
         }else{
-            meta.setDisplayName("&6Selected type: &3" + reward.getType());
+            builder.withDisplayName("&6Selected type: &3" + reward.getType());
         }
-        sound.setItemMeta(meta);
+        ItemStack sound = builder.build();
         
-        ItemStack volume = new ItemStack(Material.EMERALD);
-        meta = volume.getItemMeta();
-        double truncated = BigDecimal.valueOf(reward.getVolume()).setScale(3, RoundingMode.HALF_UP).doubleValue();
-        meta.setDisplayName("&aSound volume: &5" + truncated);
-        volume.setItemMeta(meta);
+        double value = BigDecimal.valueOf(reward.getVolume())
+                .setScale(3, RoundingMode.HALF_UP)
+                .doubleValue();
+        ItemStack volume = ItemBuilder.newItem(XMaterial.EMERALD)
+                .withDisplayName("&aSound volume: &5" + value)
+                .build();
         
-        ItemStack pitch = new ItemStack(Material.GOLD_NUGGET);
-        meta = pitch.getItemMeta();
-        truncated = BigDecimal.valueOf(reward.getPitch()).setScale(3, RoundingMode.HALF_UP).doubleValue();
-        meta.setDisplayName("&eSound pitch: &5" + truncated);
-        pitch.setItemMeta(meta);
         
-        ItemStack plus = XMaterial.LIME_STAINED_GLASS_PANE.parseItem();
-        meta = plus.getItemMeta();
-        meta.setDisplayName("&a&l+");
-        plus.setItemMeta(meta);
+        value = BigDecimal.valueOf(reward.getPitch()).setScale(3, RoundingMode.HALF_UP).doubleValue();
+        ItemStack pitch = ItemBuilder.newItem(XMaterial.GOLD_NUGGET)
+                .withDisplayName("&eSound pitch: &5" + value)
+                .build();
         
-        ItemStack less = XMaterial.RED_STAINED_GLASS_PANE.parseItem();
-        meta = less.getItemMeta();
-        meta.setDisplayName("&c&l-");
-        less.setItemMeta(meta);
-        
-        ItemStack back = new ItemStack(Material.ENDER_PEARL);
-        meta = back.getItemMeta();
-        meta.setDisplayName("&7Back");
-        back.setItemMeta(meta);
-        
-        ItemStack next = new ItemStack(Material.ANVIL);
-        meta = next.getItemMeta();
-        meta.setDisplayName("&bNext");
-        next.setItemMeta(meta);
-        
-        inv.setItem(10, back);
-        inv.setItem(16, next);
+        inv.setItem(10, GUIItem.getBackItem());
+        inv.setItem(16, GUIItem.getNextItem());
         
         inv.setItem(12, sound);
         inv.setItem(13, volume);
         inv.setItem(14, pitch);
         
-        inv.setItem(22, less);
-        inv.setItem(4, plus);
+        inv.setItem(22, GUIItem.getPlusLessItem(-1));
+        inv.setItem(4, GUIItem.getPlusLessItem(+1));
         
-        inv.setItem(23, less);
-        inv.setItem(5, plus);
+        inv.setItem(23, GUIItem.getPlusLessItem(-1));
+        inv.setItem(5, GUIItem.getPlusLessItem(+1));
         
         p.openInventory(inv);
 //</editor-fold>
@@ -197,53 +175,29 @@ public class SoundMenu{
         
         Inventory inv = GUIFactory.createInventory(CustomInventoryType.SOUND_TYPE_MENU, 54, "&3&lSound Type");
         
-        ItemStack back = new ItemStack(Material.ENDER_PEARL);
-        ItemMeta meta = back.getItemMeta();
-        meta.setDisplayName("&2Back");
-        back.setItemMeta(meta);
-        
-        ItemStack previousPage = new ItemStack(Material.MAGMA_CREAM);
-        meta = previousPage.getItemMeta();
-        meta.setDisplayName("&7Previous page");
-        previousPage.setItemMeta(meta);
-        
-        ItemStack nextPage = XMaterial.ENDER_EYE.parseItem();
-        meta = nextPage.getItemMeta();
-        meta.setDisplayName("&bNext page");
-        nextPage.setItemMeta(meta);
-        
-        ItemStack pages = new ItemStack(Material.BOOK);
-        pages.setAmount(index+1);
-        meta = pages.getItemMeta();
-        meta.setDisplayName("&6Page &5" + (index+1) + " &6of &d" + max_pages);
-        pages.setItemMeta(meta);
-        
-        if(NewAmazingLuckyBlocks.getMinecraftVersion().compareTo(MinecraftVersion.v1_9) > 0){
-            ItemStack stop = new ItemStack(Material.BARRIER);
-            meta = stop.getItemMeta();
-            meta.setDisplayName("&cStop all sounds");
-            stop.setItemMeta(meta);
+        if(NewAmazingLuckyBlocks.getMinecraftVersion().compareTo(MinecraftVersion.v1_10) >= 0){
+            ItemStack stop = ItemBuilder.newItem(XMaterial.BARRIER)
+                    .withDisplayName("&cStop all sounds")
+                    .build();
             
             inv.setItem(48, stop);
         }
         
-        inv.setItem(45, back);
+        inv.setItem(45, GUIItem.getBackItem());
         
-        inv.setItem(51, previousPage);
-        inv.setItem(52, pages);
-        inv.setItem(53, nextPage);
+        inv.setItem(51, GUIItem.getPreviousPageItem());
+        inv.setItem(52, GUIItem.getCurrentPageItem(index+1, max_pages));
+        inv.setItem(53, GUIItem.getNextPageItem());
         
         for(int i=45*index;(i-45*index)<MENU_SIZE && i<soundList.length;i++){
-            ItemStack stack = new ItemStack(Material.NOTE_BLOCK);
-            meta = stack.getItemMeta();
-            meta.setDisplayName("&3" + soundList[i].name());
-            if(reward.getType() != null && soundList[i].name().equalsIgnoreCase(reward.getType())){
-                meta.addEnchant(Enchantment.DURABILITY, 1, true);
-                meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            ItemBuilder builder = ItemBuilder.newItem(XMaterial.NOTE_BLOCK);
+            builder.withDisplayName("&3" + soundList[i].name());
+            if(reward.getType() != null && soundList[i].name().equals(reward.getType())){
+                builder.addEnchantment(Enchantment.DURABILITY, 1);
+                builder.addItemFlags(ItemFlag.HIDE_ENCHANTS);
             }
-            stack.setItemMeta(meta);
             
-            inv.setItem((i-45*index), stack);
+            inv.setItem((i-45*index), builder.build());
         }
         
         p.openInventory(inv);
@@ -286,10 +240,11 @@ public class SoundMenu{
                     openSoundTypeMenu(p);
                     break;
                 default:
-                    if(e.getCurrentItem() != null && e.getCurrentItem().hasItemMeta()){
-                        ItemMeta meta = e.getCurrentItem().getItemMeta();
-                        if(meta.hasDisplayName()){
-                            reward.setType(Logger.stripColor(meta.getDisplayName()));
+                    if(e.getCurrentItem() != null){
+                        String displayName = ItemBuilder.fromItem(e.getCurrentItem(), false)
+                                .getDisplayName();
+                        if(displayName != null){
+                            reward.setType(Logger.stripColor(displayName));
                             reward.execute(p, p.getLocation());
                             openSoundTypeMenu(p);
                         }
