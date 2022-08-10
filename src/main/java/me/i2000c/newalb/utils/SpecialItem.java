3583f2262bb3d7ec57c72fb016f8ff8164349635
I2000C <prueb123i@outlook.es>
2022-08-10 14:26:02 +0200

    Updated SpecialItem.java

--- src/main/java/me/i2000c/newalb/utils/SpecialItem.java
+++ src/main/java/me/i2000c/newalb/utils/SpecialItem.java
@@ -1,6 +1,8 @@
 package me.i2000c.newalb.utils;
 
+import me.i2000c.newalb.lang_utils.LangLoader;
 import me.i2000c.newalb.listeners.interact.PlayerInteractListener;
+import me.i2000c.newalb.utils.logger.Logger;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -21,9 +23,28 @@ public abstract class SpecialItem{
         this.item = PlayerInteractListener.setSpecialtemID(this.item, id);
     }
     
+    public String getPermissionPath(){
+        if(getSpecialItemName().isWand()){
+            return "Wands." + this.getClass().getSimpleName();
+        }else{
+            return "Objects." + this.getClass().getSimpleName();
+        }
+    }
+    
+    public String getDisplayName(){
+        String namePath;
+        if(getSpecialItemName().isWand()){
+            namePath = "Wands." + this.getClass().getSimpleName() + ".name";
+        }else{
+            namePath = "Objects." + this.getClass().getSimpleName() + ".name";
+        }
+        Logger.log(namePath);
+        return LangLoader.getMessages().getString(namePath);
+    }
+    
     public abstract ItemStack buildItem();
     
     public abstract void onPlayerInteract(PlayerInteractEvent e);
     
-    public abstract String getPermissionPath();
+    public abstract SpecialItemName getSpecialItemName();
 }
