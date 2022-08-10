d090430bec6d0156b07149f4cd287369f9e594c6
I2000C <prueb123i@outlook.es>
2022-08-10 21:25:52 +0200

    Updated LuckyTool.java

--- src/main/java/me/i2000c/newalb/listeners/objects/LuckyTool.java
+++ src/main/java/me/i2000c/newalb/listeners/objects/LuckyTool.java
@@ -1,51 +1,28 @@
 package me.i2000c.newalb.listeners.objects;
 
-import java.util.HashMap;
-import java.util.UUID;
-import me.i2000c.newalb.MinecraftVersion;
-import me.i2000c.newalb.NewAmazingLuckyBlocks;
+import com.cryptomorin.xseries.XMaterial;
 import me.i2000c.newalb.custom_outcomes.utils.LuckyBlockType;
 import me.i2000c.newalb.custom_outcomes.utils.TypeManager;
-import me.i2000c.newalb.utils.ConfigManager;
 import me.i2000c.newalb.lang_utils.LangLoader;
-import me.i2000c.newalb.utils.logger.Logger;
 import me.i2000c.newalb.listeners.interact.SpecialItem;
-import me.i2000c.newalb.utils.WorldList;
-import me.i2000c.newalb.utils2.OtherUtils;
+import me.i2000c.newalb.listeners.interact.SpecialItemName;
+import me.i2000c.newalb.utils.ConfigManager;
+import me.i2000c.newalb.utils.logger.Logger;
+import me.i2000c.newalb.utils2.ItemBuilder;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class LuckyTool extends SpecialItem{
-    private final HashMap<UUID, Long> luckyToolCooldown = new HashMap();
     
     @Override
     public void onPlayerInteract(PlayerInteractEvent e){
-        Player player = e.getPlayer();       
-        Action action = e.getAction();
-    
-        if(NewAmazingLuckyBlocks.getMinecraftVersion() != MinecraftVersion.v1_8){
-            if(e.getHand() == EquipmentSlot.OFF_HAND){
-                return;
-            }
-        }
+        Player player = e.getPlayer();
         
-        ItemStack stack = player.getItemInHand();
-        if((action.equals(Action.RIGHT_CLICK_BLOCK))){
-            ItemStack object = getItem();
-            if(!OtherUtils.checkItemStack(object, stack)){
-                return;
-            }
-            
-            if(!WorldList.isRegistered(player.getWorld().getName())){
-                return;
-            }
-            
+        if(e.getAction() == Action.RIGHT_CLICK_BLOCK){            
             TypeManager.Result result = TypeManager.canBreakBlock(player, e.getClickedBlock().getLocation());
             LuckyBlockType type;
             switch(result.resultCode){
@@ -56,22 +33,12 @@ public class LuckyTool extends SpecialItem{
                     Logger.sendMessage(LangLoader.getMessages().getString("NoPermission"), player);
                     return;
                 default:
+                    e.setCancelled(true);
                     type = result.resultType;
             }
             
-            if(!ConfigManager.getConfig().getBoolean("LuckyTool.enable")){
+            if(!ConfigManager.getConfig().getBoolean("Objects.LuckyTool.enable")){
                 Logger.sendMessage(LangLoader.getMessages().getString("Objects.LuckyTool.disabled"), player);
-                return;
-            }
-            
-            if(!OtherUtils.checkPermission(player, "LuckyTool")){
-                return;
-            }
-            
-            if((this.luckyToolCooldown.containsKey(player.getUniqueId())) && ((this.luckyToolCooldown.get(player.getUniqueId())) > System.currentTimeMillis())){
-                long remainingTime = this.luckyToolCooldown.get(player.getUniqueId()) - System.currentTimeMillis();
-                String cmsg = LangLoader.getMessages().getString("Cooldown-message").replace("%time%", String.valueOf(remainingTime / 1000L));
-                player.sendMessage(cmsg);
                 return;
             }
             
@@ -80,20 +47,19 @@ public class LuckyTool extends SpecialItem{
             target.setType(Material.AIR);
             type.executeRandomPack(e.getPlayer(), target.getLocation().add(0.5, 0, 0.5));
             
-            
-            int tw = ConfigManager.getConfig().getInt("LuckyTool.cooldown-time");
-            this.luckyToolCooldown.put(player.getUniqueId(), System.currentTimeMillis() + tw * 1000);
+            super.updatePlayerCooldown(player);
         }
     }
     
     @Override
     public ItemStack buildItem(){
-        ItemStack stack = new ItemStack(Material.STICK);
-        
-        ItemMeta meta = stack.getItemMeta();
-        meta.setDisplayName(LangLoader.getMessages().getString("Objects.LuckyTool.name"));
-        stack.setItemMeta(meta);
-        
-        return stack;
+        return ItemBuilder.newItem(XMaterial.STICK)
+                .withDisplayName(getDisplayName())
+                .build();
+    }
+    
+    @Override
+    public SpecialItemName getSpecialItemName(){
+        return SpecialItemName.luckytool;
     }
 }
