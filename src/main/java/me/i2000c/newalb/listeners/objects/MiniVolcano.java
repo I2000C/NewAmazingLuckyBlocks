05a6ce041b8fd962dddb4b6804df46ae33700a48
I2000C <prueb123i@outlook.es>
2022-08-10 17:06:14 +0200

    Updated MiniVolcano.java

--- src/main/java/me/i2000c/newalb/listeners/objects/MiniVolcano.java
+++ src/main/java/me/i2000c/newalb/listeners/objects/MiniVolcano.java
@@ -1,75 +1,36 @@
 package me.i2000c.newalb.listeners.objects;
 
-import me.i2000c.newalb.MinecraftVersion;
-import me.i2000c.newalb.NewAmazingLuckyBlocks;
-import me.i2000c.newalb.lang_utils.LangLoader;
-import me.i2000c.newalb.utils.logger.Logger;
+import com.cryptomorin.xseries.XMaterial;
 import me.i2000c.newalb.listeners.interact.SpecialItem;
-import me.i2000c.newalb.utils2.OtherUtils;
+import me.i2000c.newalb.listeners.interact.SpecialItemName;
+import me.i2000c.newalb.utils.Timer;
+import me.i2000c.newalb.utils2.ItemBuilder;
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import me.i2000c.newalb.utils.WorldList;
-import me.i2000c.newalb.utils.Timer;
-import org.bukkit.inventory.EquipmentSlot;
 
 public class MiniVolcano extends SpecialItem{
     
     @Override
     public void onPlayerInteract(PlayerInteractEvent e){
-        Player player = e.getPlayer();
-        Action action = e.getAction();
-        
-        ItemStack object = getItem();
-        String miniVName = object.getItemMeta().getDisplayName();
-
-        if(NewAmazingLuckyBlocks.getMinecraftVersion() != MinecraftVersion.v1_8){
-            if(e.getHand() == EquipmentSlot.OFF_HAND){
-                return;
-            }
-        }
-    
-        ItemStack stack = player.getItemInHand();
-        if((action.equals(Action.RIGHT_CLICK_BLOCK)) && (stack.getType() == object.getType())){
-            if(!WorldList.isRegistered(player.getWorld().getName())){
-                return;
-            }
-            if(!OtherUtils.checkPermission(player, "Objects.MiniVolcano")){
-                return;
-            }
-        
-            if(MaterialChecker.check(e)){
-                return;
-            }
-        
-            if((stack.hasItemMeta()) && (stack.getItemMeta().getDisplayName().equals(miniVName))){
-                e.setCancelled(true);
-                Location l = e.getClickedBlock().getLocation();          
-                int amt = stack.getAmount() - 1;
-                if(amt == 0){
-                    player.setItemInHand(null);
-                }else{
-                    stack.setAmount(amt);
-                }
-
-                Timer.getTimer().executeMiniVolcano(player, l);           
-            }
+        if(e.getAction() == Action.RIGHT_CLICK_BLOCK){
+            SpecialItem.decreaseAmountOfItem(e);
+            
+            Location location = e.getClickedBlock().getLocation();
+            Timer.getTimer().executeMiniVolcano(e.getPlayer(), location);
         }
     }
     
     @Override
     public ItemStack buildItem(){
-        ItemStack stack = new ItemStack(Material.LAVA_BUCKET);
-        
-        ItemMeta meta = stack.getItemMeta();
-        meta.setDisplayName(LangLoader.getMessages().getString("Objects.MiniVolcano.name"));
-        stack.setItemMeta(meta);
-        
-        return stack;
+        return ItemBuilder.newItem(XMaterial.LAVA_BUCKET)
+                .withDisplayName(getDisplayName())
+                .build();
+    }
+    
+    @Override
+    public SpecialItemName getSpecialItemName(){
+        return SpecialItemName.mini_volcano;
     }
 }
