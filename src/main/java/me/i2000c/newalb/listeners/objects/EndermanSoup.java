55613443127861b466ff81eac665a815aebb368a
I2000C <prueb123i@outlook.es>
2022-08-10 17:02:05 +0200

    Updated EndermanSoup.java

--- src/main/java/me/i2000c/newalb/listeners/objects/EndermanSoup.java
+++ src/main/java/me/i2000c/newalb/listeners/objects/EndermanSoup.java
@@ -1,78 +1,30 @@
 package me.i2000c.newalb.listeners.objects;
 
+import com.cryptomorin.xseries.XMaterial;
 import com.cryptomorin.xseries.XSound;
-import java.util.HashMap;
-import java.util.UUID;
-import me.i2000c.newalb.MinecraftVersion;
-import me.i2000c.newalb.NewAmazingLuckyBlocks;
-import me.i2000c.newalb.utils.ConfigManager;
-import me.i2000c.newalb.lang_utils.LangLoader;
-import me.i2000c.newalb.utils.logger.Logger;
 import me.i2000c.newalb.listeners.interact.SpecialItem;
-import me.i2000c.newalb.utils.WorldList;
-import me.i2000c.newalb.utils2.OtherUtils;
+import me.i2000c.newalb.listeners.interact.SpecialItemName;
+import me.i2000c.newalb.utils.ConfigManager;
+import me.i2000c.newalb.utils2.ItemBuilder;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.util.Vector;
 import xyz.xenondevs.particle.ParticleEffect;
 
-public class EndermanSoup extends SpecialItem{    
-    private final HashMap<UUID, Long> cooldown = new HashMap();
+public class EndermanSoup extends SpecialItem{
     
     @Override
     public void onPlayerInteract(PlayerInteractEvent e){
-        Player player = e.getPlayer();       
-        Action action = e.getAction();
-    
-        if(NewAmazingLuckyBlocks.getMinecraftVersion() != MinecraftVersion.v1_8){
-            if(e.getHand() == EquipmentSlot.OFF_HAND){
-                return;
-            }
-        }
-        
-        ItemStack object = getItem();
-        ItemStack stack = player.getItemInHand();
-        if((action.equals(Action.RIGHT_CLICK_AIR))){
-            if(!OtherUtils.checkItemStack(object, stack)){
-                return;
-            }
-            
-            if(!WorldList.isRegistered(player.getWorld().getName())){
-                return;
-            }
-            if(!OtherUtils.checkPermission(player, "Objects.EndermanSoup")){
-                return;
-            }
-            
-            if(MaterialChecker.check(e)){
-                return;
-            }
-            
+        if(e.getAction() == Action.RIGHT_CLICK_AIR){            
             e.setCancelled(true);
-            int dw = ConfigManager.getConfig().getInt("Objects.EndermanSoup.cooldown-time");
-            if((this.cooldown.containsKey(player.getUniqueId())) && ((this.cooldown.get(player.getUniqueId())) > System.currentTimeMillis())){
-                long remainingTime = (this.cooldown.get(player.getUniqueId())) - System.currentTimeMillis();
-                String cmsg = LangLoader.getMessages().getString("Cooldown-message").replace("%time%", String.valueOf(remainingTime / 1000L));
-                player.sendMessage(cmsg);
-                return;
-            }else{
-                this.cooldown.put(player.getUniqueId(), System.currentTimeMillis() + dw * 1000);
-            }
+            super.updatePlayerCooldown(e.getPlayer());
 
-            int amt = stack.getAmount() - 1;
-            if(amt == 0){
-                player.setItemInHand(null);
-            }else{
-                stack.setAmount(amt);
-            }
-
+            SpecialItem.decreaseAmountOfItem(e);
+            
             Player p = e.getPlayer();
             Vector v = p.getEyeLocation().getDirection();
             double multiplier = ConfigManager.getConfig().getDouble("Objects.EndermanSoup.speedMultiplier");
@@ -88,12 +40,13 @@ public class EndermanSoup extends SpecialItem{
     
     @Override
     public ItemStack buildItem(){
-        ItemStack stack = new ItemStack(Material.RABBIT_STEW);
-        
-        ItemMeta meta = stack.getItemMeta();
-        meta.setDisplayName(LangLoader.getMessages().getString("Objects.EndermanSoup.name"));
-        stack.setItemMeta(meta);
-        
-        return stack;
+        return ItemBuilder.newItem(XMaterial.RABBIT_STEW)
+                .withDisplayName(getDisplayName())
+                .build();
+    }
+    
+    @Override
+    public SpecialItemName getSpecialItemName(){
+        return SpecialItemName.enderman_soup;
     }
 }
