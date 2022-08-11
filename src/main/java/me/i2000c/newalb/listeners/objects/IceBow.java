5d52aa82b36d71c04eb0c2baf4dc50471d0e3b42
I2000C <prueb123i@outlook.es>
2022-08-11 12:38:59 +0200

    Updated IceBow.java

--- src/main/java/me/i2000c/newalb/listeners/objects/IceBow.java
+++ src/main/java/me/i2000c/newalb/listeners/objects/IceBow.java
@@ -1,14 +1,13 @@
 package me.i2000c.newalb.listeners.objects;
 
+import com.cryptomorin.xseries.XMaterial;
 import me.i2000c.newalb.NewAmazingLuckyBlocks;
-import me.i2000c.newalb.lang_utils.LangLoader;
-import me.i2000c.newalb.utils.BowItem;
+import me.i2000c.newalb.listeners.interact.CustomProjectileHitEvent;
+import me.i2000c.newalb.listeners.interact.SpecialItem;
+import me.i2000c.newalb.listeners.interact.SpecialItemName;
 import me.i2000c.newalb.utils.ConfigManager;
-import me.i2000c.newalb.utils.logger.Logger;
 import me.i2000c.newalb.utils.Timer;
-import me.i2000c.newalb.utils.WorldList;
-import me.i2000c.newalb.utils2.OtherUtils;
-import me.i2000c.newalb.utils2.Task;
+import me.i2000c.newalb.utils2.ItemBuilder;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -16,37 +15,25 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
-import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.metadata.FixedMetadataValue;
 
-
-public class IceBow extends BowItem{
-    private static final String TAG = "ice_bow";
+public class IceBow extends SpecialItem{
     
-    @EventHandler
-    public void onArrowHitEntity(EntityDamageByEntityEvent e){    
-        Entity damagee = e.getEntity();
-        Entity damager = e.getDamager();
-        String iceName = LangLoader.getMessages().getString("Objects.IceBow.name");
-
-        if(damager instanceof Arrow){
-            Arrow arrow = (Arrow) damager;
-            if(!WorldList.isRegistered(arrow.getWorld().getName())) {
-                return;
-            }
+    @Override
+    public void onArrowHit(CustomProjectileHitEvent e){
+        Entity projectile = e.getDamager();
+        if(projectile instanceof Arrow){
+            Arrow arrow = (Arrow) projectile;
+            Entity hitEntity = e.getHitEntity();
+            Block hitBlock = e.getHitBlock();
             
-            if(arrow.hasMetadata(TAG)){
-                arrow.setMetadata(TAG, new FixedMetadataValue(NewAmazingLuckyBlocks.getInstance(), false));
+            if(hitEntity != null){
+                Entity damagee = e.getEntity();
                 if(ConfigManager.getConfig().getBoolean("Objects.IceBow.disableArrowKnockback")){
                     e.setCancelled(true);
-                    ((Damageable) damagee).damage(e.getDamage(), e.getDamager());
-                    damager.remove();
+                    ((Damageable) damagee).damage(e.getDamage(), arrow);
+                    arrow.remove();
                 }
                 damagee.setFireTicks(0);
                 Timer.getTimer().executeIceBow(damagee);
@@ -54,64 +41,16 @@ public class IceBow extends BowItem{
                 if(!ConfigManager.getConfig().getBoolean("Objects.IceBow.generateSnow")){
                     e.getEntity().removeMetadata("NewAmazingLuckyBlocks", NewAmazingLuckyBlocks.getInstance());
                 }
+            }else if(hitBlock != null){
+                double radius = ConfigManager.getConfig().getDouble("Objects.IceBow.snowRadius");
+                simulateSnow(hitBlock.getLocation(), radius);
             }
         }
     }
     
-    @EventHandler
-    private void onArrowHit(ProjectileHitEvent e){
-        if(!ConfigManager.getConfig().getBoolean("Objects.IceBow.generateSnow")){
-            return;
-        }
-        
-        Entity entity = e.getEntity();
-        if(entity.hasMetadata(TAG)){
-            /*//https://www.spigotmc.org/threads/get-the-blockface-a-projectile-hit-a-block-at.77104/#post-850867
-            Location loc = entity.getLocation();
-            Vector vec = entity.getVelocity();
-            Location loc2 = new Location(loc.getWorld(), loc.getX()+vec.getX(), loc.getY()+vec.getY(), loc.getZ()+vec.getZ());
-            Block block = loc2.getBlock();
-            if(!block.isEmpty()){
-                if(block.getRelative(0, 1, 0).isEmpty()){
-                    Block b = block.getRelative(0, 1, 0);
-                    int radius = 2;
-                    this.simulateSnow(b.getLocation(), radius);
-                }
-            }*/
-            Task.runTask(() -> {
-                if(entity.getMetadata(TAG).isEmpty()){
-                    return;
-                }
-                
-                boolean blockHit = entity.getMetadata(TAG).get(0).asBoolean();
-                if(blockHit){
-                    double radius = ConfigManager.getConfig().getDouble("Objects.IceBow.snowRadius");
-                    simulateSnow(entity.getLocation().getBlock().getLocation(), radius);
-                }
-                entity.removeMetadata(TAG, NewAmazingLuckyBlocks.getInstance());
-            }, 2L);
-        }
-    }
-    
-    @EventHandler
-    private void onArrowShooted(EntityShootBowEvent e){
-        if(e.getEntity() != null && e.getEntity() instanceof Player){
-            Player shooter = (Player) e.getEntity();
-            ItemStack item = e.getBow();
-            if(item != null && item.hasItemMeta()){
-                ItemMeta meta = item.getItemMeta();
-                if(meta.hasDisplayName()){
-                    ItemStack object = getItem();
-                    String name = object.getItemMeta().getDisplayName();
-                    if(item.getType().equals(Material.BOW) && meta.getDisplayName().equalsIgnoreCase(name)){                        
-                        if(OtherUtils.checkPermission(shooter, "Objects.IceBow")){
-                            Arrow arrow = (Arrow) e.getProjectile();
-                            arrow.setMetadata(TAG, new FixedMetadataValue(NewAmazingLuckyBlocks.getInstance(), true));
-                        }                           
-                    }
-                }
-            }
-        }
+    @Override
+    public void onArrowShooted(EntityShootBowEvent e){
+        super.setClassMetadata(e.getProjectile());
     }
     
     private static void simulateSnow(Location loc, double radius){
@@ -157,13 +96,14 @@ public class IceBow extends BowItem{
     
     @Override
     public ItemStack buildItem(){
-        ItemStack stack = new ItemStack(Material.BOW);
-        
-        ItemMeta meta = stack.getItemMeta();
-        meta.setDisplayName(LangLoader.getMessages().getString("Objects.IceBow.name"));
-        meta.addEnchant(Enchantment.DURABILITY, 5, true);
-        stack.setItemMeta(meta);
-        
-        return stack;
+        return ItemBuilder.newItem(XMaterial.BOW)
+                .withDisplayName(getDisplayName())
+                .addEnchantment(Enchantment.DURABILITY, 5)
+                .build();
+    }
+    
+    @Override
+    public SpecialItemName getSpecialItemName(){
+        return SpecialItemName.ice_bow;
     }
 }
