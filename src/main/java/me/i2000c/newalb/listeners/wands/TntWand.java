61012a1c8e9c532c4daf7cb613e8a4b9f968d765
I2000C <prueb123i@outlook.es>
2022-08-11 19:44:28 +0200

    Updated TntWand.java

--- src/main/java/me/i2000c/newalb/listeners/wands/TntWand.java
+++ src/main/java/me/i2000c/newalb/listeners/wands/TntWand.java
@@ -1,123 +1,42 @@
 package me.i2000c.newalb.listeners.wands;
 
 import com.cryptomorin.xseries.XMaterial;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
-import me.i2000c.newalb.NewAmazingLuckyBlocks;
-import me.i2000c.newalb.listeners.objects.MaterialChecker;
-import me.i2000c.newalb.MinecraftVersion;
-import me.i2000c.newalb.utils.ConfigManager;
-import me.i2000c.newalb.lang_utils.LangLoader;
-import me.i2000c.newalb.utils.logger.Logger;
 import me.i2000c.newalb.listeners.interact.SpecialItem;
-import me.i2000c.newalb.utils2.OtherUtils;
+import me.i2000c.newalb.utils2.ItemBuilder;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.util.Vector;
 
-import me.i2000c.newalb.utils.WorldList;
-import org.bukkit.inventory.EquipmentSlot;
-
 public class TntWand extends SpecialItem{
-    private final HashMap<UUID, Long> tntcooldown = new HashMap();
     
     @Override
     public void onPlayerInteract(PlayerInteractEvent e){
-        Player player = e.getPlayer();
-        Action action = e.getAction();
-        
-        ItemStack wand = getItem();
-        String tntName = wand.getItemMeta().getDisplayName();
-
-        if(NewAmazingLuckyBlocks.getMinecraftVersion() != MinecraftVersion.v1_8){
-            if(e.getHand() == EquipmentSlot.OFF_HAND){
-                return;
-            }
-        }
-        
-        ItemStack stack = player.getItemInHand();
-        int tw = ConfigManager.getConfig().getInt("Wands.TNT.cooldown-time");
-        double v = 4.5D;
-        if(((action == Action.RIGHT_CLICK_AIR) || (action == Action.RIGHT_CLICK_BLOCK)) && (player.getItemInHand().getType() == wand.getType())){
-            if(!WorldList.isRegistered(player.getWorld().getName())){
-                return;
-            }
-            if(!OtherUtils.checkPermission(player, "Wands.TNT")){
-                return;
-            }
+        if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK){
+            e.setCancelled(true);
             
-            if(MaterialChecker.check(e)){
-                return;
-            }else{
+            Player player = e.getPlayer();
+            if(!super.decreaseWandUses(e.getItem(), e.getPlayer())){
                 e.setCancelled(true);
+                return;
             }
             
-            if((stack.hasItemMeta()) && (stack.getItemMeta().getDisplayName().equals(tntName))){
-                if((this.tntcooldown.containsKey(player.getUniqueId())) && ((this.tntcooldown.get(player.getUniqueId())) > System.currentTimeMillis())){
-                    long remainingTime = ((Long)this.tntcooldown.get(player.getUniqueId())).longValue() - System.currentTimeMillis();
-                    String cmsg = LangLoader.getMessages().getString("Cooldown-message").replace("%time%", String.valueOf(remainingTime / 1000L));
-                    player.sendMessage(cmsg);
-                }else{
-                    ItemMeta meta = stack.getItemMeta();
-                    if(ConfigManager.getConfig().getBoolean("Wands.TNT.limited-uses.enable")){
-                        if(meta.hasLore()){
-                            List<String> loreList = meta.getLore();
-                            int uses = Integer.parseInt(loreList.get(1));
-                            if(uses == 0){
-                              player.sendMessage("&cThis wand has expired");
-                              return;
-                            }else{
-                              uses--;
-                            }
-                            loreList.set(1, String.valueOf(uses));
-                            meta.setLore(loreList);
-                            stack.setItemMeta(meta);
-                        }else{
-                            List<String> loreList = new ArrayList();
-                            int uses = ConfigManager.getConfig().getInt("Wands.TNT.limited-uses.uses");
-                            loreList.add("Uses left:");
-                            loreList.add(String.valueOf(uses));
-                            meta.setLore(loreList);
-                            stack.setItemMeta(meta);
-                        } 
-                    }else{
-                        if(meta.hasLore()){
-                            List<String> loreList = meta.getLore();
-                            loreList.clear();
-                            meta.setLore(loreList);
-                            stack.setItemMeta(meta);
-                        }
-                    }  
-                    
-                    this.tntcooldown.put(player.getUniqueId(), System.currentTimeMillis() + tw * 1000);
-                    Vector direction = player.getEyeLocation().getDirection().multiply(v);
-                    TNTPrimed tnt = (TNTPrimed)player.getWorld().spawn(player.getEyeLocation().add(direction.getX(), direction.getY(), direction.getZ()), TNTPrimed.class);
-                    tnt.setVelocity(player.getLocation().getDirection());
-                }
-            }
+            super.updatePlayerCooldown(player);
+            Location eyeLocation = player.getEyeLocation();
+            Vector direction = eyeLocation.getDirection().multiply(4.5);
+            TNTPrimed tnt = (TNTPrimed) player.getWorld().spawn(eyeLocation.add(direction), TNTPrimed.class);
+            tnt.setVelocity(eyeLocation.getDirection());
         }
     }
     
     @Override
     public ItemStack buildItem(){
-        ItemStack stack = XMaterial.MUSIC_DISC_BLOCKS.parseItem();
-        ItemMeta meta = stack.getItemMeta();
-        meta.setDisplayName(LangLoader.getMessages().getString("Wands.TNT.name"));
-        if(ConfigManager.getConfig().getBoolean("Wands.TNT.limited-uses.enable")){
-            int uses = ConfigManager.getConfig().getInt("Wands.TNT.limited-uses.uses");
-            List<String> loreList = new ArrayList();
-            loreList.add("Uses left:");
-            loreList.add(String.valueOf(uses));
-            meta.setLore(loreList);
-        }
-        stack.setItemMeta(meta);
-        
-        return stack;
+        return ItemBuilder.newItem(XMaterial.MUSIC_DISC_BLOCKS)
+                .withDisplayName(getDisplayName())
+                .withLore(super.getLoreOfWand())
+                .build();
     }
 }
