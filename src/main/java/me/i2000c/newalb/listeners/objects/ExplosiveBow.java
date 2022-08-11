bfe17372d32ebae53e968812118239717a86f18c
I2000C <prueb123i@outlook.es>
2022-08-11 12:48:05 +0200

    Updated ExplosiveBow.java

--- src/main/java/me/i2000c/newalb/listeners/objects/ExplosiveBow.java
+++ src/main/java/me/i2000c/newalb/listeners/objects/ExplosiveBow.java
@@ -1,70 +1,46 @@
 package me.i2000c.newalb.listeners.objects;
 
-import me.i2000c.newalb.NewAmazingLuckyBlocks;
+import com.cryptomorin.xseries.XMaterial;
+import me.i2000c.newalb.listeners.interact.CustomProjectileHitEvent;
+import me.i2000c.newalb.listeners.interact.SpecialItemName;
 import me.i2000c.newalb.utils.BowItem;
 import me.i2000c.newalb.utils.ConfigManager;
-import me.i2000c.newalb.lang_utils.LangLoader;
-import me.i2000c.newalb.utils.logger.Logger;
-import me.i2000c.newalb.utils2.OtherUtils;
+import me.i2000c.newalb.utils2.ItemBuilder;
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Arrow;
-import org.bukkit.event.EventHandler;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.entity.EntityShootBowEvent;
-import org.bukkit.event.entity.ProjectileHitEvent;
-import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.inventory.ItemStack;
 
 
 public class ExplosiveBow extends BowItem{
-    private static final String TAG = "explosive_bow";
     
-    @EventHandler
-    private void onArrowHit(ProjectileHitEvent e){
-        if(e.getEntity().hasMetadata(TAG)){
-            float power = (float) ConfigManager.getConfig().getDouble("Objects.ExplosiveBow.explosionPower");
-            boolean withFire = ConfigManager.getConfig().getBoolean("Objects.ExplosiveBow.createFire");
-            boolean breakBlocks = ConfigManager.getConfig().getBoolean("Objects.ExplosiveBow.breakBlocks");
-            Location loc = e.getEntity().getLocation();
-            e.getEntity().getWorld().createExplosion(loc.getX(), loc.getY(), loc.getZ(), power, withFire, breakBlocks);
-            e.getEntity().removeMetadata("NewAmazingLuckyBlocks", NewAmazingLuckyBlocks.getInstance());
-            e.getEntity().remove();
-        }
+    @Override
+    public void onArrowHit(CustomProjectileHitEvent e){
+        Entity projectile = e.getDamager();
+        float power = (float) ConfigManager.getConfig().getDouble("Objects.ExplosiveBow.explosionPower");
+        boolean withFire = ConfigManager.getConfig().getBoolean("Objects.ExplosiveBow.createFire");
+        boolean breakBlocks = ConfigManager.getConfig().getBoolean("Objects.ExplosiveBow.breakBlocks");
+        Location loc = projectile.getLocation();
+        loc.getWorld().createExplosion(loc.getX(), loc.getY(), loc.getZ(), power, withFire, breakBlocks);
+        projectile.remove();
     }
   
-    @EventHandler
-    private void onArrowShooted(EntityShootBowEvent e){
-        if(e.getEntity() != null && e.getEntity() instanceof Player){
-            Player shooter = (Player) e.getEntity();
-            ItemStack item = e.getBow();
-            if(item != null && item.hasItemMeta()){
-                ItemMeta meta = item.getItemMeta();
-                if(meta.hasDisplayName()){
-                    ItemStack object = getItem();
-                    String name = object.getItemMeta().getDisplayName();
-                    if(item.getType().equals(Material.BOW) && meta.getDisplayName().equalsIgnoreCase(name)){                        
-                        if(OtherUtils.checkPermission(shooter, "Objects.ExplosiveBow")){
-                            Arrow arrow = (Arrow) e.getProjectile();
-                            arrow.setMetadata(TAG, new FixedMetadataValue(NewAmazingLuckyBlocks.getInstance(), TAG));
-                        }                           
-                    }
-                }
-            }
-        }
+    @Override
+    public void onArrowShooted(EntityShootBowEvent e){
+        super.setClassMetadata(e.getProjectile());
     }
     
     @Override
     public ItemStack buildItem(){
-        ItemStack stack = new ItemStack(Material.BOW);
-        
-        ItemMeta meta = stack.getItemMeta();
-        meta.setDisplayName(LangLoader.getMessages().getString("Objects.ExplosiveBow.name"));
-        meta.addEnchant(Enchantment.ARROW_DAMAGE, 1, true);
-        stack.setItemMeta(meta);
-        
-        return stack;
+        return ItemBuilder.newItem(XMaterial.BOW)
+                .withDisplayName(getDisplayName())
+                .addEnchantment(Enchantment.ARROW_DAMAGE, 1)
+                .build();
+    }
+    
+    @Override
+    public SpecialItemName getSpecialItemName(){
+        return SpecialItemName.explosive_bow;
     }
 }
