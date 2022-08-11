20668e150469eb35e2ecc0b3647473982f02a150
I2000C <prueb123i@outlook.es>
2022-08-11 19:58:29 +0200

    Updated LightningWand.java

--- src/main/java/me/i2000c/newalb/listeners/wands/LightningWand.java
+++ src/main/java/me/i2000c/newalb/listeners/wands/LightningWand.java
@@ -1,122 +1,38 @@
 package me.i2000c.newalb.listeners.wands;
 
 import com.cryptomorin.xseries.XMaterial;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
-import me.i2000c.newalb.NewAmazingLuckyBlocks;
-import me.i2000c.newalb.listeners.objects.MaterialChecker;
-import me.i2000c.newalb.MinecraftVersion;
-import me.i2000c.newalb.utils.ConfigManager;
-import me.i2000c.newalb.lang_utils.LangLoader;
-import me.i2000c.newalb.utils.logger.Logger;
 import me.i2000c.newalb.listeners.interact.SpecialItem;
-import me.i2000c.newalb.utils2.OtherUtils;
-import org.bukkit.Material;
+import me.i2000c.newalb.utils2.ItemBuilder;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import me.i2000c.newalb.utils.WorldList;
-import org.bukkit.inventory.EquipmentSlot;
 
 public class LightningWand extends SpecialItem{
-    private final HashMap<UUID, Long> lightningcooldown = new HashMap();
     
     @Override
     public void onPlayerInteract(PlayerInteractEvent e){
-        Player player = e.getPlayer();
-        Action action = e.getAction();
-        
-        ItemStack wand = getItem();
-        String lightName = wand.getItemMeta().getDisplayName();
-
-        if(NewAmazingLuckyBlocks.getMinecraftVersion() != MinecraftVersion.v1_8){
-            if(e.getHand() == EquipmentSlot.OFF_HAND){
-                return;
-            }
-        }
-        
-        ItemStack stack = player.getItemInHand();
-        int lw = ConfigManager.getConfig().getInt("Wands.Lightning.cooldown-time");
-        if(((action == Action.RIGHT_CLICK_AIR) || (action == Action.RIGHT_CLICK_BLOCK)) && (player.getItemInHand().getType() == wand.getType())){
-            if(!WorldList.isRegistered(player.getWorld().getName())){
-                return;
-            }
-            if(!OtherUtils.checkPermission(player, "Wands.Lightning")){
-                return;
-            }
+        if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK){
+            e.setCancelled(true);
             
-            if(MaterialChecker.check(e)){
-                return;
-            }else{
+            Player player = e.getPlayer();
+            if(!super.decreaseWandUses(e.getItem(), e.getPlayer())){
                 e.setCancelled(true);
+                return;
             }
             
-            if((stack.hasItemMeta()) && (stack.getItemMeta().getDisplayName().equals(lightName))){
-                if ((this.lightningcooldown.containsKey(player.getUniqueId())) && ((this.lightningcooldown.get(player.getUniqueId())) > System.currentTimeMillis())){
-                    long remainingTime = (this.lightningcooldown.get(player.getUniqueId())) - System.currentTimeMillis();
-                    String cmsg = LangLoader.getMessages().getString("Cooldown-message").replace("%time%", String.valueOf(remainingTime / 1000L));
-                    player.sendMessage(cmsg);
-                }else{
-                    this.lightningcooldown.put(player.getUniqueId(), System.currentTimeMillis() + lw * 1000);
-                    Block block = player.getTargetBlock((Set<Material>) null, 120);
-                    
-                    ItemMeta meta = stack.getItemMeta();
-                    if(ConfigManager.getConfig().getBoolean("Wands.Lightning.limited-uses.enable")){
-                        if(meta.hasLore()){
-                            List<String> loreList = meta.getLore();
-                            int uses = Integer.parseInt(loreList.get(1));
-                            if(uses == 0){
-                              player.sendMessage("&cThis wand has expired");
-                              return;
-                            }else{
-                              uses--;
-                            }
-                            loreList.set(1, String.valueOf(uses));
-                            meta.setLore(loreList);
-                            stack.setItemMeta(meta);
-                        }else{
-                            List<String> loreList = new ArrayList();
-                            int uses = ConfigManager.getConfig().getInt("Wands.Lightning.limited-uses.uses");
-                            loreList.add("Uses left:");
-                            loreList.add(String.valueOf(uses));
-                            meta.setLore(loreList);
-                            stack.setItemMeta(meta);
-                        } 
-                    }else{
-                        if(meta.hasLore()){
-                            List<String> loreList = meta.getLore();
-                            loreList.clear();
-                            meta.setLore(loreList);
-                            stack.setItemMeta(meta);
-                        }
-                    }
-                    player.getWorld().strikeLightning(block.getLocation());
-                }
-            }
-        }
+            super.updatePlayerCooldown(player);
+            Block block = player.getTargetBlock(null, 120);
+            player.getWorld().strikeLightning(block.getLocation());
+        }        
     }
     
     @Override
     public ItemStack buildItem(){
-        ItemStack stack = XMaterial.MUSIC_DISC_FAR.parseItem();
-        ItemMeta meta = stack.getItemMeta();
-        meta.setDisplayName(LangLoader.getMessages().getString("Wands.Lightning.name"));
-        if(ConfigManager.getConfig().getBoolean("Wands.Lightning.limited-uses.enable")){
-            int uses = ConfigManager.getConfig().getInt("Wands.Lightning.limited-uses.uses");
-            List<String> loreList = new ArrayList();
-            loreList.add("Uses left:");
-            loreList.add(String.valueOf(uses));
-            meta.setLore(loreList);
-        }
-        stack.setItemMeta(meta);
-        
-        return stack;
+        return ItemBuilder.newItem(XMaterial.MUSIC_DISC_FAR)
+                .withDisplayName(getDisplayName())
+                .withLore(super.getLoreOfWand())
+                .build();
     }
 }
