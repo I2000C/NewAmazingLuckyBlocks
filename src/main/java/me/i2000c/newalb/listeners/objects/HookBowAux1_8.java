aa2f1a1d1666d665e1e58d9630e5c849b0879eca
I2000C <prueb123i@outlook.es>
2023-12-13 20:49:38 +0100

    Added new Reflection system to HookBowAux1_8.java

--- src/main/java/me/i2000c/newalb/listeners/objects/HookBowAux1_8.java
+++ src/main/java/me/i2000c/newalb/listeners/objects/HookBowAux1_8.java
@@ -1,49 +1,15 @@
 package me.i2000c.newalb.listeners.objects;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import me.i2000c.newalb.utils2.OtherUtils;
+import me.i2000c.newalb.reflection.ReflectionManager;
 import me.i2000c.newalb.utils2.Task;
 import org.bukkit.Location;
 import org.bukkit.entity.Chicken;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-public class HookBowAux1_8{
+public class HookBowAux1_8 {
     
-    private static Method getHandle;
-    
-    private static Method getNBTTag;
-    private static Field invulnerableField;
-    private static Method c;
-    private static Method f;
-    
-    private static Constructor nbtTagCompoundConstructor;
-    private static Method setInt;
-    
-    static{
-        //<editor-fold defaultstate="collapsed" desc="Code">
-        try{
-            getHandle = OtherUtils.getCraftClass("entity.CraftEntity").getMethod("getHandle");
-            
-            Class nmsEntityClass = OtherUtils.getNMSClass(null, "Entity");
-            getNBTTag = nmsEntityClass.getMethod("getNBTTag");
-            invulnerableField = nmsEntityClass.getDeclaredField("invulnerable");
-            
-            Class nbtTagCompoundClass = OtherUtils.getNMSClass(null, "NBTTagCompound");
-            nbtTagCompoundConstructor = nbtTagCompoundClass.getConstructor();
-            
-            setInt = nbtTagCompoundClass.getMethod("setInt", String.class, int.class);
-            c = nmsEntityClass.getMethod("c", nbtTagCompoundClass);
-            f = nmsEntityClass.getMethod("f", nbtTagCompoundClass);
-        }catch(Exception ex){
-            ex.printStackTrace();
-        }
-//</editor-fold>
-    }
-    
-    public static Chicken createEntityChicken(Location loc, boolean isInvisible, boolean isInvulnerable, boolean withAI, boolean isSilent) throws Exception{
+    public static Chicken createEntityChicken(Location loc, boolean isInvisible, boolean isInvulnerable, boolean withAI, boolean isSilent) {
         Chicken entity;
         if(isInvisible){
             entity = loc.getWorld().spawn(loc.clone().add(0, 200, 0), Chicken.class);
@@ -59,25 +25,18 @@ public class HookBowAux1_8{
         //https://bukkit.org/threads/send-packets-to-a-player.117055/
 
         //https://www.spigotmc.org/threads/how-to-make-a-horse-invulnerable-unable-to-move-invisible-and-silent.500750/
-        Object nmsEntity = getHandle.invoke(entity);
-        Object nbtTagCompound = getNBTTag.invoke(nmsEntity);
+        Object nmsEntity = ReflectionManager.callMethod(entity, "getHandle");
+        Object nbtTagCompound = ReflectionManager.callMethod(nmsEntity, "getNBTTag");
         if(nbtTagCompound == null){
-            nbtTagCompound = nbtTagCompoundConstructor.newInstance();
+            nbtTagCompound = ReflectionManager.getCachedNMSClass(null, "NBTTagCompound").callConstructor();
         }
         
-        c.invoke(nmsEntity, nbtTagCompound);
-        if(!withAI){
-            setInt.invoke(nbtTagCompound, "NoAI", 1);
-        }
-        if(isSilent){
-            setInt.invoke(nbtTagCompound, "Silent", 1);
-        }
-        f.invoke(nmsEntity, nbtTagCompound);
+        ReflectionManager.callMethod(nmsEntity, "c", nbtTagCompound);
+        if(!withAI) ReflectionManager.callMethod(nbtTagCompound, "setInt", "NoAI", 1);
+        if(isSilent) ReflectionManager.callMethod(nbtTagCompound, "setInt", "Silent", 1);
+        ReflectionManager.callMethod(nmsEntity, "f", nbtTagCompound);
         
-        if(isInvulnerable){
-            invulnerableField.setAccessible(true);
-            invulnerableField.set(nmsEntity, true);
-        }
+        if(isInvulnerable) ReflectionManager.getCachedNMSClass(null, "Entity").setFieldValue("invulnerable", nmsEntity, true);
         
         return entity;
     }
