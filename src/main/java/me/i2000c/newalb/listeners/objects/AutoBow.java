0a809a7fbaab0499972e286e86bf300c3afc40a2
I2000C <prueb123i@outlook.es>
2023-12-19 12:46:54 +0100

    Improved AutoBow.java and added BowUtils.java

--- src/main/java/me/i2000c/newalb/listeners/objects/AutoBow.java
+++ src/main/java/me/i2000c/newalb/listeners/objects/AutoBow.java
@@ -1,32 +1,22 @@
 package me.i2000c.newalb.listeners.objects;
 
 import com.cryptomorin.xseries.XMaterial;
-import com.cryptomorin.xseries.XSound;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import me.i2000c.newalb.MinecraftVersion;
-import me.i2000c.newalb.NewAmazingLuckyBlocks;
+import java.util.Optional;
 import me.i2000c.newalb.listeners.interact.SpecialItem;
+import me.i2000c.newalb.listeners.objects.utils.BowUtils;
 import me.i2000c.newalb.utils.ConfigManager;
 import me.i2000c.newalb.utils2.ItemBuilder;
-import me.i2000c.newalb.utils2.OtherUtils;
-import me.i2000c.newalb.utils2.Task;
 import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.SpectralArrow;
-import org.bukkit.entity.TippedArrow;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.potion.PotionData;
 import org.bukkit.util.Vector;
 
-public class AutoBow extends SpecialItem{
+public class AutoBow extends SpecialItem {
+    
+    private double multiplier;
     
     @Override
     public void onPlayerInteract(PlayerInteractEvent e){
@@ -35,174 +25,33 @@ public class AutoBow extends SpecialItem{
             e.setCancelled(true);
             
             Player player = e.getPlayer();
-            ItemStack stack = e.getItem();
+            ItemStack bowStack = e.getItem();
             
-            boolean withFire = stack.getEnchantments().containsKey(Enchantment.ARROW_FIRE);
+            boolean isFireBow = BowUtils.isFireBow(bowStack);
+            boolean isInfiniteBow = BowUtils.isInfiniteBow(player, bowStack);
             
-            int slot = -1;
-            for(int i=0;i<player.getInventory().getContents().length;i++){
-                ItemStack sk = player.getInventory().getItem(i);
-                if(sk == null){
-                    continue;
-                }
-                if(sk.getType().name().contains("ARROW")){
-                    slot = i;
-                    break;
-                }
-            }
-            
-            ItemStack arrowItem;
-            if(slot != -1){
-                arrowItem = player.getInventory().getItem(slot).clone();
-            }else{
-                arrowItem = new ItemStack(Material.ARROW);
-            }
-            
-            if(player.getGameMode() == GameMode.CREATIVE){
-                launchArrow(player, true, withFire, arrowItem);
+            ItemStack arrowStack;
+            Optional<ItemStack> arrowStackOptional = BowUtils.getArrowFromPlayerInventory(player, !isInfiniteBow);
+            if(arrowStackOptional.isPresent()) {
+                arrowStack = arrowStackOptional.get();
+            } else if(player.getGameMode() == GameMode.CREATIVE) {
+                arrowStack = ItemBuilder.newItem(XMaterial.ARROW).build();
+            } else {
                 return;
             }
             
-            if(slot != -1){
-                if(!stack.getEnchantments().containsKey(Enchantment.DURABILITY)){
-                    stack.setDurability((short)(stack.getDurability()+6));
-                }else{
-                    int level = stack.getEnchantments().get(Enchantment.DURABILITY);
-                    if(level < 3){
-                        stack.setDurability((short)(stack.getDurability()+6-level*2));
-                    }
-                }
-                if(384-stack.getDurability() <= 0){
-                    player.playSound(player.getLocation(), XSound.ENTITY_ITEM_BREAK.parseSound(), 20, 1);
-                    player.setItemInHand(null);
-                    return;
-                }
-                
-                
-                if(stack.getEnchantments().containsKey(Enchantment.ARROW_INFINITE)){
-                    launchArrow(player, true, withFire, arrowItem);
-                }else{
-                    ItemStack arrow = player.getInventory().getItem(slot);
-                    int amt = arrow.getAmount();
-                    if(amt > 1){
-                        arrow.setAmount(amt - 1);
-                    }else{
-                        player.getInventory().setItem(slot, null);
-                    }
-                    launchArrow(player, false, withFire, arrowItem);
-                }
-            }
-        }
-//</editor-fold>
-    }
-    
-    private static Method setBasePotionMeta;
-    
-    private static Method getArrowHandle;
-    private static Class<?> entityArrow;
-    private static Field fromPlayer;
-    private static Object pickupStatus = null;
-    private static boolean initialized = false;
-    
-    private void launchArrow(Player player, boolean infinityBow, boolean fireBow, ItemStack arrowStack){
-        double multiplier = ConfigManager.getConfig().getDouble(super.itemPathKey + ".velocityMultiplier");
-        Vector velocity = player.getLocation().getDirection().multiply(multiplier);
-        
-        //<editor-fold defaultstate="collapsed" desc="Code">
-        try{
-            if(!initialized){
-                if(!NewAmazingLuckyBlocks.getMinecraftVersion().isLegacyVersion()){
-                    if(NewAmazingLuckyBlocks.getMinecraftVersion() != MinecraftVersion.v1_13){
-                        setBasePotionMeta = Arrow.class.getMethod("setBasePotionData", PotionData.class);
-                    }
-                }
-                
-                getArrowHandle = OtherUtils.getCraftClass("entity.CraftArrow").getMethod("getHandle");
-                entityArrow = OtherUtils.getNMSClass("net.minecraft.world.entity.projectile", "EntityArrow");
-                try{
-                    fromPlayer = entityArrow.getField("fromPlayer");
-                }catch(NoSuchFieldException ex){
-                    //Since Minecraft 1.17, "fromPlayer" field has been called "d"
-                    fromPlayer = entityArrow.getField("d");
-                }
-                
-                initialized = true;
-            }
-        }catch(Exception ex){
-            ex.printStackTrace();
-        }
-        
-        ItemStack sk = player.getItemInHand();
-        player.setItemInHand(sk);
-        if(!NewAmazingLuckyBlocks.getMinecraftVersion().isLegacyVersion()){
-            player.setItemInHand(null);
-            Task.runTask(() -> player.setItemInHand(sk), 2);
-        }
-        
-        Arrow arrow;
-        switch(arrowStack.getType()){
-            case SPECTRAL_ARROW:
-                SpectralArrow sa = player.launchProjectile(SpectralArrow.class, velocity);
-                if(fireBow){
-                    //In 1.16 SpectralArrow cannot be casted to Arrow
-                    sa.setFireTicks(2000);
-                }
-                arrow = (Arrow) sa;
-                break;
-            case TIPPED_ARROW:
-                try{
-                    EntityType et = EntityType.TIPPED_ARROW;
-                    TippedArrow ta = (TippedArrow) player.launchProjectile(TippedArrow.class, velocity);
-                    PotionMeta meta = (PotionMeta) arrowStack.getItemMeta();
-                    ta.setBasePotionData(meta.getBasePotionData());
-                    arrow = (Arrow) ta;
-                    if(fireBow){
-                        arrow.setFireTicks(2000);
-                    }
-                }catch(NoSuchFieldError | ClassCastException ex){
-                    arrow = (Arrow) player.launchProjectile(Arrow.class, velocity);
-                    //In 1.16 tipped arrow is a normal arrow with effects
-                    //Set effects
-                    try{
-                        PotionMeta meta = (PotionMeta) arrowStack.getItemMeta();
-                        setBasePotionMeta.invoke(arrow, meta.getBasePotionData());
-                        if(fireBow){
-                            arrow.setFireTicks(2000);
-                        }
-                    }catch(Exception ex2){
-                    }
-                }
-                break;
-            default:
-                arrow = player.launchProjectile(Arrow.class, velocity);
-                if(fireBow){
-                    arrow.setFireTicks(2000);
-                }
-        }
-        
-        if(infinityBow){
-            try{
-                Object craftArrow = getArrowHandle.invoke(arrow);
-                
-                if(NewAmazingLuckyBlocks.getMinecraftVersion() == MinecraftVersion.v1_8){
-                    fromPlayer.set(craftArrow, 2);
-                }else{
-                    if(pickupStatus == null){
-                        Class pickupStatusClass = OtherUtils.getNMSClass("net.minecraft.world.entity.projectile", "EntityArrow$PickupStatus");
-                        pickupStatus = pickupStatusClass.getMethod("valueOf", String.class).invoke(pickupStatusClass, "CREATIVE_ONLY");
-                    }
-                    
-                    fromPlayer.set(craftArrow, pickupStatus);
-                }
-            }catch(Exception ex){
-                ex.printStackTrace();
-            }
+            Vector velocity = player.getLocation().getDirection().multiply(multiplier);
+            BowUtils.launchArrow(player, arrowStack, isFireBow, isInfiniteBow, velocity);
+            BowUtils.applyDurability(player, bowStack);
+            BowUtils.cancelBowCharging(player);
         }
 //</editor-fold>
     }
     
     @Override
     public ItemStack buildItem(){
+        this.multiplier = ConfigManager.getConfig().getDouble(super.itemPathKey + ".velocityMultiplier");
+        
         return ItemBuilder.newItem(XMaterial.BOW)
                 .addEnchantment(Enchantment.ARROW_DAMAGE, 1)
                 .build();
---
+++ src/main/java/me/i2000c/newalb/listeners/objects/utils/BowUtils.java
@@ -0,0 +1,204 @@
+package me.i2000c.newalb.listeners.objects.utils;
+
+import com.cryptomorin.xseries.XMaterial;
+import com.cryptomorin.xseries.XSound;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
+import me.i2000c.newalb.MinecraftVersion;
+import me.i2000c.newalb.reflection.RefClass;
+import me.i2000c.newalb.reflection.ReflectionManager;
+import me.i2000c.newalb.utils2.Task;
+import org.bukkit.GameMode;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.SpectralArrow;
+import org.bukkit.entity.TippedArrow;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionData;
+import org.bukkit.util.Vector;
+
+public class BowUtils {
+    
+    private static final int DURABILITY_PER_USE = 6;
+    private static final int DURABILITY_RECOVERED_PER_LEVEL = -2;
+    private static final int MAX_DURABILITY_LEVEL = 3;
+    private static final int MAX_DURABILITY_VALUE_BOW = 384;
+    
+    private static final int ARROW_FIRE_TICKS = 2000;
+    
+    private static final EnumSet ARROW_MATERIALS = EnumSet.of(XMaterial.ARROW, 
+                                                              XMaterial.TIPPED_ARROW, 
+                                                              XMaterial.SPECTRAL_ARROW);
+    
+    public static boolean isFireBow(ItemStack bowStack) {
+        return bowStack.getEnchantments().containsKey(Enchantment.ARROW_FIRE);
+    }
+    
+    public static boolean isInfiniteBow(Player player, ItemStack bowStack) {
+        return player.getGameMode() == GameMode.CREATIVE
+                || bowStack.getEnchantments().containsKey(Enchantment.ARROW_INFINITE);
+    }
+    
+    public static boolean applyDurability(Player player, ItemStack bowStack) {
+        //<editor-fold defaultstate="collapsed" desc="Code">
+        if(player.getGameMode() == GameMode.CREATIVE) {
+            return true;
+        }
+        
+        int durability = bowStack.getDurability();
+        if(bowStack.getEnchantments().containsKey(Enchantment.DURABILITY)) {
+            int level = bowStack.getEnchantments().get(Enchantment.DURABILITY);
+            if(level < MAX_DURABILITY_LEVEL) {
+                durability = durability + DURABILITY_PER_USE - level*DURABILITY_RECOVERED_PER_LEVEL;
+            }
+        } else {
+            durability += DURABILITY_PER_USE;
+        }
+        
+        if(MAX_DURABILITY_VALUE_BOW - durability <= 0) {
+            XSound.ENTITY_ITEM_BREAK.play(player);
+            player.setItemInHand(null);
+            return false;
+        } else {
+            bowStack.setDurability((short) durability);
+            return true;
+        }
+//</editor-fold>
+    }
+    
+    public static void launchArrow(Player player, 
+                                    ItemStack arrowStack, 
+                                    boolean isFireBow, boolean isInfiniteBow, 
+                                    Vector velocity) {
+        //<editor-fold defaultstate="collapsed" desc="Code">
+        Arrow arrow;
+        XMaterial xmaterial = XMaterial.matchXMaterial(arrowStack);
+        switch(xmaterial) {
+            case SPECTRAL_ARROW:
+                arrow = player.launchProjectile(SpectralArrow.class, velocity);
+                break;
+            case TIPPED_ARROW:
+                PotionData potionData = null;
+                if(arrowStack.getItemMeta() instanceof PotionMeta) {
+                    PotionMeta meta = (PotionMeta) arrowStack.getItemMeta();
+                    potionData = meta.getBasePotionData();
+                }
+                
+                if(MinecraftVersion.getCurrentVersion().compareTo(MinecraftVersion.v1_16) >= 0) {
+                    // Since Minecraft 1.16, tipped arrows are normal arrows with effects
+                    arrow = player.launchProjectile(Arrow.class, velocity);
+                    if(potionData != null) {
+                        ReflectionManager.callMethod(arrow, "setBasePotionMeta", potionData);
+                    }
+                } else {
+                    // From Minecraft 1.9 to 1.15, tipped arrows are instances of TippedArrow
+                    arrow = player.launchProjectile(TippedArrow.class, velocity);
+                    if(potionData != null) {
+                        ((TippedArrow) arrow).setBasePotionData(potionData);
+                    }
+                }
+                break;
+            default:
+                arrow = player.launchProjectile(Arrow.class, velocity);
+        }
+        
+        if(isFireBow) {
+            arrow.setFireTicks(ARROW_FIRE_TICKS);
+        }
+        
+        if(isInfiniteBow) {
+            ArrowPickupStatus.CREATIVE_ONLY.setToArrow(arrow);
+        }
+//</editor-fold>
+    }
+    
+    public static List<ItemStack> getArrowsFromPlayerInventory(Player player, int requiredAmount, boolean removeItems) {
+        //<editor-fold defaultstate="collapsed" desc="Code">
+        List<ItemStack> arrowItemList = new ArrayList<>();
+        ItemStack[] items = player.getInventory().getContents();
+        
+        for(int i=0; i<items.length && requiredAmount > 0; i++) {
+            ItemStack item = items[i];
+            if(item == null) {
+                continue;
+            }
+            
+            XMaterial xmaterial = XMaterial.matchXMaterial(item);
+            if(!ARROW_MATERIALS.contains(xmaterial)) {
+                continue;
+            }
+            
+            ItemStack copy = item.clone();
+            copy.setAmount(1);
+            
+            int itemAmount = item.getAmount();
+            if(itemAmount > requiredAmount) {
+                for(int j=0; j<requiredAmount; j++) {
+                    arrowItemList.add(copy);
+                }
+                
+                if(removeItems) item.setAmount(itemAmount - requiredAmount);
+            } else {
+                for(int j=0; j<itemAmount; j++) {
+                    arrowItemList.add(copy);
+                }
+                if(removeItems) player.getInventory().setItem(i, null);
+            }
+            
+            requiredAmount -= itemAmount;
+        }
+        
+        return arrowItemList;
+//</editor-fold>
+    }
+    public static Optional<ItemStack> getArrowFromPlayerInventory(Player player, boolean removeItems) {
+        //<editor-fold defaultstate="collapsed" desc="Code">
+        List<ItemStack> arrowItems = getArrowsFromPlayerInventory(player, 1, removeItems);
+        return arrowItems.stream().findFirst();
+//</editor-fold>
+    }
+    
+    public static void cancelBowCharging(Player player) {
+        //<editor-fold defaultstate="collapsed" desc="Code">
+        ItemStack stack = player.getItemInHand();
+        if(stack == null) {
+            return;
+        }
+        
+        if(MinecraftVersion.getCurrentVersion().isLegacyVersion()) {
+            player.setItemInHand(stack);
+        } else {
+            player.setItemInHand(null);
+            Task.runTask(() -> player.setItemInHand(stack), 2L);
+        }
+//</editor-fold>
+    }    
+    
+    public static enum ArrowPickupStatus {
+        DISALLOWED, ALLOWED, CREATIVE_ONLY;
+        
+        public void setToArrow(Arrow arrow) {
+            //<editor-fold defaultstate="collapsed" desc="Code">
+            Object craftArrow = ReflectionManager.callMethod(arrow, "getHandle");
+            if(MinecraftVersion.getCurrentVersion() == MinecraftVersion.v1_8) {
+                // In Minecraft 1.8, the field is called "fromPlayer" and requires an int (0, 1 or 2)
+                ReflectionManager.setFieldValue(craftArrow, "fromPlayer", this.ordinal());
+            } else {
+                RefClass refClass = ReflectionManager.getNMSClass("net.minecraft.world.entity.projectile", "EntityArrow$PickupStatus");
+                Object pickupStatus = refClass.callStaticMethod("valueOf", this.name());
+                if(MinecraftVersion.getCurrentVersion().compareTo(MinecraftVersion.v1_17) >= 0) {
+                    // Since Minecraft 1.17, the field is called "d"
+                    ReflectionManager.setFieldValue(craftArrow, "d", pickupStatus);
+                } else {
+                    // From Minecraft 1.9 to Minecraft 1.16, the field is called "fromPlayer", but requires an enum
+                    ReflectionManager.setFieldValue(craftArrow, "fromPlayer", pickupStatus);
+                }
+            }
+//</editor-fold>
+        }
+    }
+}
