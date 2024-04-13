d3c725bfd2a403bab0f039aba80044e24df4b4b9
I2000C <prueb123i@outlook.es>
2024-04-13 20:59:14 +0200

    Fixed bug that caused that Players weren't able to chat in a Mohist server

--- src/main/java/me/i2000c/newalb/listeners/chat/ChatListener.java
+++ src/main/java/me/i2000c/newalb/listeners/chat/ChatListener.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener{
     private static final Map<Player, ChatFunction> players = new HashMap<>();
@@ -33,7 +33,7 @@ public class ChatListener implements Listener{
     }
     
     @EventHandler(priority = EventPriority.LOWEST)
-    private static void onPlayerChat(PlayerChatEvent e){
+    private static void onPlayerChat(AsyncPlayerChatEvent e){
         if(players.isEmpty()){
             return;
         }
