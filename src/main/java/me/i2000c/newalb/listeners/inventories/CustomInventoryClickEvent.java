e1e8bb701c28e96892d33b1c28f097dc87b3dea6
I2000C <prueb123i@outlook.es>
2026-04-19 12:35:55 +0200

    Simplified CustomInventoryClickEvent

--- src/main/java/me/i2000c/newalb/listeners/inventories/CustomInventoryClickEvent.java
+++ src/main/java/me/i2000c/newalb/listeners/inventories/CustomInventoryClickEvent.java
@@ -1,163 +1,52 @@
 package me.i2000c.newalb.listeners.inventories;
 
-import com.google.common.base.Objects;
+import java.util.Objects;
 
-import me.i2000c.newalb.api.gui.InventoryLocation;
-
-import java.util.List;
-import org.bukkit.entity.HumanEntity;
-import org.bukkit.event.Event;
-import org.bukkit.event.HandlerList;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 
-public class CustomInventoryClickEvent{
-    private final InventoryClickEvent event;
-    private final InventoryLocation location;
+import lombok.Getter;
+import lombok.experimental.Delegate;
+import me.i2000c.newalb.api.gui.InventoryLocation;
+
+public class CustomInventoryClickEvent {
     
-    public CustomInventoryClickEvent(InventoryClickEvent e){
-        this.event = e;
-        InventoryView view = e.getView();        
-        if(view == null){
+    @Delegate private final InventoryClickEvent event;
+    @Getter private final InventoryLocation location;
+    @Getter private final boolean topInventory;
+    @Getter private final boolean bottomInventory;
+    @Getter private final boolean noneInventory;
+    @Getter private final boolean emptyItem;
+    @Getter private final Player player;
+    
+    public CustomInventoryClickEvent(InventoryClickEvent event) {
+        this.event = event;
+        
+        InventoryView view = event.getView();        
+        if(view == null) {
             this.location = InventoryLocation.NONE;
-        }else{
-            Inventory inventory = e.getClickedInventory();
-            if(Objects.equal(inventory, view.getTopInventory())){
+        } else {
+            Inventory inventory = event.getClickedInventory();
+            if(Objects.equals(inventory, view.getTopInventory())) {
                 this.location = InventoryLocation.TOP;
-            }else if(Objects.equal(inventory, view.getBottomInventory())){
+            } else if(Objects.equals(inventory, view.getBottomInventory())) {
                 this.location = InventoryLocation.BOTTOM;
-            }else{
+            } else {
                 this.location = InventoryLocation.NONE;
             }
         }
+        
+        this.topInventory = this.location == InventoryLocation.TOP;
+        this.bottomInventory = this.location == InventoryLocation.BOTTOM;
+        this.noneInventory = this.location == InventoryLocation.NONE;
+        
+        ItemStack currentItem = event.getCurrentItem();
+        this.emptyItem = currentItem == null || currentItem.getType() == Material.AIR;
+        
+        this.player = (Player) event.getWhoClicked();
     }
-    
-    public InventoryLocation getLocation(){
-        return this.location;
-    }
-
-    public Inventory getClickedInventory() {
-        return event.getClickedInventory();
-    }
-
-    public org.bukkit.event.inventory.InventoryType.SlotType getSlotType() {
-        return event.getSlotType();
-    }
-
-    public ItemStack getCursor() {
-        return event.getCursor();
-    }
-
-    public ItemStack getCurrentItem() {
-        return event.getCurrentItem();
-    }
-
-    public boolean isRightClick() {
-        return event.isRightClick();
-    }
-
-    public boolean isLeftClick() {
-        return event.isLeftClick();
-    }
-
-    public boolean isShiftClick() {
-        return event.isShiftClick();
-    }
-
-    public void setCursor(ItemStack stack) {
-        event.setCursor(stack);
-    }
-
-    public void setCurrentItem(ItemStack stack) {
-        event.setCurrentItem(stack);
-    }
-
-    public int getSlot() {
-        return event.getSlot();
-    }
-
-    public int getRawSlot() {
-        return event.getRawSlot();
-    }
-
-    public int getHotbarButton() {
-        return event.getHotbarButton();
-    }
-
-    public InventoryAction getAction() {
-        return event.getAction();
-    }
-
-    public ClickType getClick() {
-        return event.getClick();
-    }
-
-    public HandlerList getHandlers() {
-        return event.getHandlers();
-    }
-
-    public static HandlerList getHandlerList() {
-        return InventoryClickEvent.getHandlerList();
-    }
-
-    public HumanEntity getWhoClicked() {
-        return event.getWhoClicked();
-    }
-
-    public void setResult(Event.Result newResult) {
-        event.setResult(newResult);
-    }
-
-    public Event.Result getResult() {
-        return event.getResult();
-    }
-
-    public boolean isCancelled() {
-        return event.isCancelled();
-    }
-
-    public void setCancelled(boolean toCancel) {
-        event.setCancelled(toCancel);
-    }
-
-    public Inventory getInventory() {
-        return event.getInventory();
-    }
-
-    public List<HumanEntity> getViewers() {
-        return event.getViewers();
-    }
-
-    public InventoryView getView() {
-        return event.getView();
-    }
-
-    public String getEventName() {
-        return event.getEventName();
-    }
-
-    public final boolean isAsynchronous() {
-        return event.isAsynchronous();
-    }
-
-    @Override
-    public int hashCode() {
-        return event.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return event.equals(obj);
-    }
-
-    @Override
-    public String toString() {
-        return event.toString();
-    }
-    
-    
 }
